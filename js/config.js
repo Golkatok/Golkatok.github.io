7febade24ee0d1058238f8cee474216c1a135f95
Golkatok <illiatablet90@gmail.com>
2025-11-26 19:18:25 +0100

    Update and rename axel-ai.js to config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// js/config.js
+const CONFIG = {
+    YOUTUBE_API_KEY: "ТУТ_ТВОЙ_НОВЫЙ_КЛЮЧ",
+    YOUTUBE_CHANNEL_ID: "UCrZA2Mj6yKZkEcBIqdfF6Ag",
+    GEMINI_API_KEY: "ТУТ_ТВОЙ_НОВЫЙ_КЛЮЧ_GEMINI" // Если он отличается
+};
